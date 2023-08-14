f8df48676428c8aac0391f8d960e715c336102c9
Luigi Johns <135214189+zzjdwkljd@users.noreply.github.com>
2023-08-14 21:09:49 +0800

    Update SELENIUM_.py

--- SELENIUM_.py
+++ SELENIUM_.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+selenium 4.10
+python 3.10
+
+'''
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -23,6 +30,7 @@ class Locator:
     }
 
 class SELENIUM():
+    # 初始化,防检测
     def __init__(self, argument='Y'):
         self.option = Options()
         self.option.page_load_strategy = 'none'
@@ -35,27 +43,31 @@ class SELENIUM():
             self.option.add_argument("--headless")
         self.driver = webdriver.Chrome(options=self.option)
 
-    def get_url(self, u):
+    # 访问网址
+    def get_url(self, url: str) -> str:
         try:
             self.driver.maximize_window()
-            self.driver.get(u)
+            self.driver.get(url)
         except Exception as e:
             raise Exception(f"get_url()发生了错误: {e}")
 
+    # 获取当前窗口句柄
     def get_window(self):
         try:
             self.driver.switch_to.window(self.driver.window_handles[-1])
         except Exception as e:
             raise Exception(f"get_window()发生了错误: {e}")
 
-    def get_cookies(self, u=''):
+    # 获取cookie
+    def get_cookies(self, url: str) -> str:
         try:
-            if u:
-                self.get_url(u)
+            if url:
+                self.get_url(url)
             return self.driver.get_cookies()
         except Exception as e:
             raise Exception(f"get_cookies()发生了错误: {e}")
 
+    # 传入cookie
     def set_cookies(self, c):
         try:
             for cookie in c:
@@ -64,25 +76,28 @@ class SELENIUM():
         except Exception as e:
             raise Exception(f"set_cookies()发生了错误: {e}")
 
+    # 结束 selenium
     def close_selenium(self):
         try:
             self.driver.quit()
         except Exception as e:
             raise Exception(f"close_selenium()发生了错误: {e}")
 
+    # 单击元素[ 模拟鼠标 ]
     def click_element(self, element):
         try:
             ActionChains(self.driver).move_to_element(element).click(element).perform()
         except Exception as e:
             raise Exception(f"click_element()发生了错误: {e}")
 
-    def click_key(self, key):
+    # 单击键盘
+    def click_key(self, key: str) -> str:
         try:
-            key = str(key)
             ActionChains(self.driver).key_down(key).perform()
         except Exception as e:
             raise Exception(f"click_key()发生了错误: {e}")
 
+    # 拖动元素
     def move_element(self, element, x, y, speed=50, release=False):
         try:
             if not element.is_displayed():
@@ -96,37 +111,39 @@ class SELENIUM():
         except Exception as e:
             raise Exception(f"move_element()发生了错误: {e}")
 
+    # 查找单个元素
     def finds_element(self, **kwargs):
         try:
-            locator = ''
-            way = ''
+            locat =None
+            locator = None
+            way = None
+            element = None
             wait = WebDriverWait(self.driver, kwargs["t"] if kwargs.get("t", False) else 10)
-
             for k, v in kwargs.items():
-                if Locator.LOCATORS.get(k, False):
-                    locator = (Locator.LOCATORS.get(k, False), v)
-                way = Locator.EC_.get(k, False)
-                if not locator:
-                    return False
-                if way:
-                    if way == 'element_to_be_clickable':
-                        element = wait.until(EC.element_to_be_clickable(locator))
-                    elif way == 'visibility_of':
-                        element = wait.until(EC.visibility_of(locator))
-                    elif way == 'presence_of_element_located':
-                        element = wait.until(EC.presence_of_element_located(locator))
-                    elif way == 'visibility_of_element_located':
-                        element = wait.until(EC.visibility_of_element_located(locator))
-                    else:
-                        element = wait.until(EC.presence_of_element_located(locator))
-            else:
-                locator = False
+                if Locator.LOCATORS.get(k, None):
+                    locator = (Locator.LOCATORS.get(k, None), v)
+                if Locator.EC_.get(k, None):
+                    way = Locator.EC_.get(k, None)
 
-            if locator != False and way != False:
+            if locator and way:
+                if way == 'element_to_be_clickable':
+                    element = wait.until(EC.element_to_be_clickable(locator))
+                elif way == 'visibility_of':
+                    element = wait.until(EC.visibility_of(locator))
+                elif way == 'presence_of_element_located':
+                    element = wait.until(EC.presence_of_element_located(locator))
+                elif way == 'visibility_of_element_located':
+                    element = wait.until(EC.visibility_of_element_located(locator))
+                else:
+                    element = wait.until(EC.presence_of_element_located(locator))
+
                 return element
+
+            return False
         except Exception as e:
             raise Exception(f"finds_element()发生了错误: {e}")
 
+    # 查找多个元素
     def finds_elements(self, **kwargs):
         try:
             for k, v in kwargs.items():
@@ -136,3 +153,9 @@ class SELENIUM():
         except Exception as e:
             raise Exception(f"finds_elements()发生了错误: {e}")
 
+    # 截图
+    def save_screenshot(self, n: str = 'error') -> str:
+        try:
+            self.driver.save_screenshot(f"{n}.png")
+        except Exception as e:
+            raise Exception(f"save_screenshot()发生了错误: {e}")
